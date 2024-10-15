f4a787edb0e65cf1dba9d2532487f8f1d0bcdf99
vokrob <106639488+vokrob@users.noreply.github.com>
2024-10-15 20:43:42 +0700

    Initial commit

---
+++ app/src/main/java/com/vokrob/fishermans_handbook_2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vokrob.fishermans_handbook_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
