aeb5dd5323a88dd7d2e7fe60362587c86023e286
vokrob <106639488+vokrob@users.noreply.github.com>
2024-10-15 21:16:08 +0700

    Menu Click Listener

--- app/src/main/java/com/vokrob/fishermans_handbook_2/MainActivity.kt
+++ app/src/main/java/com/vokrob/fishermans_handbook_2/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.vokrob.fishermans_handbook_2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navView = findViewById(R.id.nav_view)
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.id_fish -> Toast.makeText(this, "Id fish", Toast.LENGTH_SHORT).show()
+            R.id.id_baits -> Toast.makeText(this, "Id baits", Toast.LENGTH_SHORT).show()
+            R.id.id_tackle -> Toast.makeText(this, "Id tackle", Toast.LENGTH_SHORT).show()
+            R.id.id_stories -> Toast.makeText(this, "Id stories", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
