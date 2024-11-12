e7006e9ec5098e532ce081e07ff55d3a1a3875cf
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-12 15:21:41 +0700

    Changing minSdkVersion from 24 to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vokrob.fishermans_handbook_2"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
